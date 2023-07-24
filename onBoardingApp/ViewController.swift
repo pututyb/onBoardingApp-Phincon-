2f381a9ff5d35b4394527387c594e216ed122596
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-07-24 13:17:12 +0700

    Initial Commit

---
+++ onBoardingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  onBoardingApp
+//
+//  Created by Putut Yusri Bahtiar on 24/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
